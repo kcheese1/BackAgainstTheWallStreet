a8569d59c70a787b9b97dd30c13cb6b3fd0dc43d
Charles <frontgoals@gmail.com>
2022-03-16 15:50:10 -0400

    sprint1v3

--- FrontEnd/BAWS/auth.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-auth = Blueprint('auth', __name__)
-
-@auth.route('/')
-def home():
-    return "<p>Login</p>"
--- FrontEnd/BAWS/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-views = Blueprint("views", __name__, template_folder = 'template')
-
-@views.route('/')
-def home():
-    return render_template("index.html")
