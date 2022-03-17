19989de15b62d3a71a3695fe4d4ac965c5eff045
Daniel Saviet <ds8653@desales.edu>
2022-03-16 22:16:58 -0400

    sprint 1 V4

---
+++ loginPage.py
@@ -0,0 +1,8 @@
+#login page
+#author: Daniel Saviet
+#Date: 3/14/2022
+#Description: Displays the login page of the app and handles interaction with the page
+
+
+from flask import Blueprint, render_template, request, flash, jsonify, Flask
+
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#kmkeslfkn slekv slkve 
-# test comment
-#test2 comment
-#this is gonna work
-#source control test
+
+
+
