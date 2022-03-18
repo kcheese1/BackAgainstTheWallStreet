aa986aaa32590598505b6cb0e023cbea34168a1d
Daniel Saviet <ds8653@desales.edu>
2022-03-18 03:08:57 -0400

    Sprint 1 final version(V12)

--- FrontEnd/BAWS/__init__.py
+++ FrontEnd/BAWS/__init__.py
@@ -5,15 +5,28 @@ import sys
 
 app = Flask(__name__)
 
+@app.route('/', methods = ["GET", "POST"])
+def getLoginData():
+    if request.method == 'POST':
+        USERNAME = str(request.form['UsernameL'])
+        PASSWORD = str(request.form['PasswordL'])
+        user_table = pd.read_csv('FrontEnd/BAWS/FileStorage/user.csv')
+        
+        if USERNAME in user_table["Username"].values and PASSWORD in user_table["Password"].values:
+             return redirect(url_for('views.index'))
+        print(user_table, file = sys.stderr)
+    
+    return render_template("loginPage.html")
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/index', methods=['GET', 'POST'])
 def home():
     return render_template('index.html')
 
 @app.route('/sign_up', methods=['GET', 'POST'])
 def user_data():
     if request.method == 'POST':
-        sign_up_file = pd.read_csv('FrontEnd/BAWS/featherFileStorage/user.csv')
+        sign_up_file = pd.read_csv('FrontEnd/BAWS/FileStorage/user.csv')
         USERNAME = str(request.form['Username'])
         PASSWORD = str(request.form['Password'])
         REPASSWORD = str(request.form['RePassword'])
@@ -21,22 +34,12 @@ def user_data():
             input_data = pd.DataFrame()
             input_data['Username'] = [USERNAME]
             input_data['Password'] = [PASSWORD]
-            print(input_data, file=sys.stderr)
-            print(sign_up_file, file=sys.stderr)
             sign_up_file = sign_up_file.append(input_data, ignore_index = True)
-            sign_up_file.to_csv('FrontEnd/BAWS/featherFileStorage/user.csv', index=False)
-        return redirect(url_for('views.home'))
+            print(sign_up_file, file=sys.stderr)
+            sign_up_file.to_csv('FrontEnd/BAWS/FileStorage/user.csv', index=False)
+            return redirect(url_for('views.home'))
     return render_template('sign_up.html')
     
-@app.route('/login', methods = ["GET", "POST"])
-def getLoginData():
-    username = "dddd"
-    password = "ssss"
-    
-    print(username, file=sys.stderr)
-    
-    
-    return render_template("loginPage.html")
 
 def create_app():
     
--- FrontEnd/BAWS/views.py
+++ FrontEnd/BAWS/views.py
@@ -4,12 +4,12 @@ views = Blueprint("views", __name__, template_folder = 'template')
 
 @views.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("loginPage.html")
 
 @views.route('/sign_up')
 def sign_up():
     return render_template("sign_up.html")
 
-@views.route('/login')
-def login():
-    return render_template("loginPage.html")
+@views.route('/index')
+def index():
+    return render_template("index.html")
